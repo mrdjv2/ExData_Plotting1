4d1e4332a36f5afc12e77a62a3e3aefb299494ff
Daniel <daniel.jungen@t-online.de>
2017-09-27 11:05:10 +0200

    Update png1.R

--- png1.R
+++ png1.R
@@ -3,4 +3,6 @@ relevant_data<-data[data[,1]== "1/2/2007" |data[,1]== "2/2/2007",]
 
 relevant_data[,3]<-as.numeric(as.character(relevant_data[,3]))
 
-hist(relevant_data$Global_active_power)
+png(file = "plot1.png", bg = "transparent",width = 480, height = 480)
+hist(relevant_data$Global_active_power, col = "red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
